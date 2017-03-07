daa2f56b86d3e839c65482b934a25b088ff9e051
Gabriel Valdez <gvaldezm@gmail.com>
2017-03-06 19:04:04 -0800

    Add 4 logic strings to comply with leap year rules - result is still no leap year

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,12 @@ $(document).ready(function(){
     $("#result").text(result);
   });
   var leapYear1 = function(year){
-    if (year % 4 === 0){
+    if (year % 100 === 0) {
+      return false;
+    } else if (year % 4 === 0){
       return true;
     } else {
       return false;
-  }
+  };
 };
 });
