aac180eb028882abe375fe76672d967f45fe899a
Gabriel Valdez <gvaldezm@gmail.com>
2017-03-06 18:58:35 -0800

    Add js logic for leap year project, create input form, capture value, give back response, does not evaluate leap year logic yet

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("form#leapyear").submit(function(event){
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear1(year);
+    $("#result").text(result);
+  });
+  var leapYear1 = function(year){
+    if (year % 4 === 0){
+      return true;
+    } else {
+      return false;
+  }
+};
+});
